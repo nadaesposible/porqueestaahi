506d43d5a7d5e6e743cb1e8ea43ebdd1ea3d1e1c
Pedro Galan <pedro.galan@piksel.com>
2016-01-29 21:27:39 +0000

    Add full website.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+blogOriginalOnMouseOver = function() {
+    document.getElementById('tachaduraBlogOriginal').style.display = 'block';
+    document.getElementById('comentarioBlogOriginal').style.display = 'block';
+}
+blogOriginalOnMouseOut = function() {
+    document.getElementById('tachaduraBlogOriginal').style.display = 'none';
+    document.getElementById('comentarioBlogOriginal').style.display = 'none';
+}
+
+blogRevisadoOnMouseOver = function() {
+    document.getElementById('tachaduraBlogRevisado').style.display = 'block';
+    document.getElementById('comentarioBlogRevisado').style.display = 'block';
+}
+blogRevisadoOnMouseOut = function() {
+    document.getElementById('tachaduraBlogRevisado').style.display = 'none';
+    document.getElementById('comentarioBlogRevisado').style.display = 'none';
+}
+
+libroOnMouseOver = function() {
+    document.getElementById('tachaduraLibro').style.display = 'block';
+    document.getElementById('comentarioLibro').style.display = 'block';
+}
+libroOnMouseOut = function() {
+    document.getElementById('tachaduraLibro').style.display = 'none';
+    document.getElementById('comentarioLibro').style.display = 'none';
+}
+
